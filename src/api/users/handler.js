3d3f685eff605d9acc0348b050c1e1a8a37e81cc
Ikhsan-kun <xsanofficial@gmail.com>
2025-08-06 22:36:47 +0700

    collaboration implemented

--- src/api/users/handler.js
+++ src/api/users/handler.js
@@ -6,6 +6,8 @@ class UsersHandler {
     this._validator = validator;
 
     this.postUserHandler = this.postUserHandler.bind(this);
+    this.getUsersByUsernameHandler = this.getUsersByUsernameHandler.bind(this);
+    this.getUserByIdHandler = this.getUserByIdHandler.bind(this);
   }
 
   async postUserHandler(request, h) {
@@ -41,7 +43,16 @@ class UsersHandler {
       },
     };
   }
+  async getUsersByUsernameHandler(request, h) {
+    const { username = "" } = request.query;
+    const users = await this._service.getUsersByUsername(username);
+    return {
+      status: "success",
+      data: {
+        users,
+      },
+    };
+  }
 }
-
 
 module.exports = UsersHandler;
--- src/services/postgres/UserService.js
+++ src/services/postgres/UserService.js
@@ -25,7 +25,14 @@ class UsersService {
     }
     return result.rows[0].id;
   }
-
+  async getUsersByUsername(username) {
+    const query = {
+      text: "SELECT id, username, fullname FROM users WHERE username LIKE $1",
+      values: [`%${username}%`],
+    };
+    const result = await this._pool.query(query);
+    return result.rows;
+  }
   async verifyNewUsername(username) {
     const query = {
       text: "SELECT username FROM users WHERE username = $1",
